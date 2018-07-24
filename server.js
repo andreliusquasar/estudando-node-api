c54239ecce3af378049e54a9cecb8f5e5c55419d
andrelius <andrelius.quasar@gmail.com>
2018-07-23 21:44:42 -0300

    Aula 3 - Configurando um servidor simples [Observacao - As aulas 1 e 2 fala sobre instalacao e compensou a partir da Aula 3]

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+
+// Configurando primeira rota
+var route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Minha API Node",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
+
+server.listen(port);
+console.log('Subindo a aplicação');
