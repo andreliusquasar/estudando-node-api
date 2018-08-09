4c92fb414ae682524727722279d2966f99c3524b
andrelius <andrelius.quasar@gmail.com>
2018-08-09 13:45:41 -0300

    Aula 14 - Realizando conexao com o mongoose

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+// Conexao com o banco
+mongoose.connect(
+  "string de conexao"
+);
+
+// Carregar Rotas
+const indexRoutes   = require('./routes/index-routes');
+const productRoutes = require('./routes/product-routes');
 
 // Utilizando o body-parser como um midway - init
 app.use(bodyParser.json());
@@ -13,9 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Utilizando o body-parser como um midway - end
 
 
-// Carregar Rotas
-const indexRoutes   = require('./routes/index-routes');
-const productRoutes = require('./routes/product-routes');
 
 // Utilizando as rotas
 app.use("/", indexRoutes);
