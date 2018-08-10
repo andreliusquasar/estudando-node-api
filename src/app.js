de78a4c4c8f51cd6ff8f1774c2474037b98bf56c
ANDRE LUIS DA SILVA <andre.luis@viavarejo.com.br>
2018-08-10 12:26:50 -0300

    Ajustando conexão local

--- src/app.js
+++ src/app.js
@@ -3,14 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const conexao = require('./conection/conection');
 
 const app = express();
 const router = express.Router();
 
 // Conexao com o banco
-mongoose.connect(
-  "string de conexao"
-);
+mongoose.connect(conexao, { useNewUrlParser: true });
 
 // Carregando Models
 const Product = require('./models/product.model');
